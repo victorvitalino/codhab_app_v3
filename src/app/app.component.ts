e3f4984fcca11b27682763f3721e0e35e5614fd9
Victor Vitalino <vitalino.victor@gmail.com>
2018-11-16 17:48:27 -0200

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,16 +14,18 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  habitationNav: boolean = false;
+  regularizationNav: boolean = false;
+  entityNav: boolean = false;
+  portalNav: boolean = false;
+  cadastreNav: boolean = false;
+  serviceNav: boolean = false;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform,
+   
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
-    ];
 
   }
 
@@ -40,5 +42,56 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+
+  // Controles Sidebar
+
+  openHabitationNav(reset = false) {
+    this.resetNav('habitationNav', this.habitationNav);
+    this.habitationNav = (reset == true) ? false : this.habitationNav
+    this.habitationNav = (this.habitationNav == true) ? false : true
+  }
+
+  openRegularizationNav(reset = false) {
+    this.resetNav('regularizationNav', this.regularizationNav);
+    this.regularizationNav = (reset == true) ? false : this.regularizationNav
+    this.regularizationNav = (this.regularizationNav == true) ? false : true
+  }
+
+  openEntityNav(reset = false) {
+    this.resetNav('entityNav', this.entityNav);
+    this.entityNav = (reset == true) ? false : this.entityNav
+    this.entityNav = (this.entityNav == true) ? false : true
+  }
+
+  openPortalNav(reset = false) {
+    this.resetNav('portalNav', this.portalNav);
+    this.portalNav = (reset == true) ? false : this.portalNav
+    this.portalNav = (this.portalNav == true) ? false : true
+  }
+
+  openCadastreNav(reset = false) {
+    this.resetNav('cadastreNav', this.cadastreNav);
+    this.cadastreNav = (reset == true) ? false : this.cadastreNav
+    this.cadastreNav = (this.cadastreNav == true) ? false : true
+  }
+
+  openServiceNav(reset = false) {
+    this.resetNav('serviceNav', this.serviceNav);
+    this.serviceNav = (reset == true) ? false : this.serviceNav
+    this.serviceNav = (this.serviceNav == true) ? false : true
+  }
+
+  resetNav(exemption_nav, value) {
+    this.habitationNav = false
+    this.regularizationNav = false
+    this.habitationNav = false
+    this.regularizationNav = false
+    this.entityNav = false
+    this.portalNav = false
+    this.cadastreNav = false
+
+    this[exemption_nav] = value
   }
 }
